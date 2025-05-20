689f0e136e31e585b0e09bd4e18a8a31692c322f
Anette Taivere <taivereanette@gmail.com>
2025-05-20 22:53:58 +0300

    fix footer and add links

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -6,17 +6,33 @@ const Footer = () => {
     return (
         <section className="c-space pt-7 pb-3 border-t
         border-black-300 flex justify-between items-center flex-wrap gap-5">
-            <div className="text-white-500 flex gap-2">
-                <p>Terms & Conditions</p>
-                <p>|</p>
-                <p>Privacy Policy</p>
+
+            <div className="flex justify-center gap-3">
+                <a href="https://github.com/AnetteTaivere/" target="_blank" rel="noopener noreferrer">
+                    <div className="social-icon">
+                        <img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2"/>
+
+                    </div>
+                </a>
+                <a href="https://www.linkedin.com/in/anette-taivere/" target="_blank" rel="noopener noreferrer">
+                    <div className="social-icon">
+                        <img src="/assets/linkedin.svg" alt="linkedin" className="w-1/2 h-1/2"/>
+                    </div>
+                </a>
             </div>
 
-            <div className="flex gap-3">
-                <div className="social-icon">
-                    <img src="/assets/github.svg" alt="github"
-                         className="w-1/2 h-1/2"/>
-                </div>
+            <div className="social-icon">
+                <button
+                    onClick={() => window.scrollTo({top: 0, behavior: 'smooth'})}
+                    aria-label="Back to top"
+                    className="social-icon"
+                >
+                    <img
+                        src="/assets/toTop.svg"
+                        alt="Back to top"
+                        className="w-1/2 h-1/2 object-contain"
+                    />
+                </button>
 
             </div>
 
